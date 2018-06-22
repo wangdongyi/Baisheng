f3395ec08186eb2169ac766a14b0b7ced7c15161
wangdongyi <wangdongyi@zmeng123.com>
2018-06-22 17:31:59 +0800

    提交

--- app/src/main/java/com/wdy/baisheng/MainActivity.java
+++ app/src/main/java/com/wdy/baisheng/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 
 import com.gizwits.opensource.appkit.UserModule.LoadingActivity;
 
+import wdy.business.util.StatusBarUtil;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
+        StatusBarUtil.setStatusBarDark(getWindow(),true);
         Intent intent=new Intent(MainActivity.this, LoadingActivity.class);
         startActivity(intent);
         finish();
